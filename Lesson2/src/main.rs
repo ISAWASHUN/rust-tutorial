f5b5ac1da733b55f634ca3bf7d1a3c85448c0c09
ISAWASHUN <syun20000616@icloud.com>
2024-04-26 23:22:43 +0900

    列挙型

--- Lesson2/src/main.rs
+++ Lesson2/src/main.rs
@@ -1,19 +1,32 @@
-fn fizzbuzz(end: i32) {
-    let mut cnt: i32 = 1;
-    while cnt <= end {
-        if cnt % 15 == 0 {
-            println!("FizzBuzz");
-        } else if cnt % 3 == 0 {
-            println!("Fizz");
-        } else if cnt % 5 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{}", cnt);
-        }
-        cnt += 1;
-    }
+enum Shape {
+    Circle,
+    Square(u32),
+    Triangle{base: u32, height: u32},
 }
 
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+
+//     fn new(width: u32, height: u32) -> Rectangle {
+//         Rectangle { width, height }
+//     }
+// }
+
 fn main() {
-    fizzbuzz(30);
+    let c: Shape = Shape::Circle;
+    let s: Shape = Shape::Square(10);
+    let t: Shape = Shape::Triangle{base: 10, height: 20};
+
+    match c {
+        Shape::Circle => println!("This is a circle"),
+        Shape::Square(side) => println!("This is a square with side {}", side),
+        Shape::Triangle{base, height} => println!("This is a triangle with base {} and height {}", base, height),
+    }
 }
