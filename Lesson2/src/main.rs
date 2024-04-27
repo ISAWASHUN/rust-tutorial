0062357ca5017c687b503aea35b40d61a52f51f4
ISAWASHUN <syun20000616@icloud.com>
2024-04-27 16:59:21 +0900

    マッチガード

--- Lesson2/src/main.rs
+++ Lesson2/src/main.rs
@@ -1,8 +1,8 @@
-enum Shape {
-    Circle,
-    Square(u32),
-    Triangle{base: u32, height: u32},
-}
+// enum Shape {
+//     Circle,
+//     Square(u32),
+//     Triangle{base: u32, height: u32},
+// }
 
 // struct Rectangle {
 //     width: u32,
@@ -20,13 +20,29 @@ enum Shape {
 // }
 
 fn main() {
-    let c: Shape = Shape::Circle;
-    let s: Shape = Shape::Square(10);
-    let t: Shape = Shape::Triangle{base: 10, height: 20};
+    // let c: Shape = Shape::Circle;
+    // let s: Shape = Shape::Square(10);
+    // let t: Shape = Shape::Triangle{base: 10, height: 20};
 
-    match c {
-        Shape::Circle => println!("This is a circle"),
-        Shape::Square(side) => println!("This is a square with side {}", side),
-        Shape::Triangle{base, height} => println!("This is a triangle with base {} and height {}", base, height),
+    // match c {
+    //     Shape::Circle => println!("This is a circle"),
+    //     Shape::Square(side) => println!("This is a square with side {}", side),
+    //     Shape::Triangle{base, height} => println!("This is a triangle with base {} and height {}", base, height),
+ // }
+
+    // let a: Option<i32> = Some(1);
+    // let b: Option<&str> = Some("str");
+    // let c: Option<i32> = None;
+    
+    let v: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let val: Option<&i32> = v.get(2);
+
+    match val {
+        Some(x) if *x == 1 => println!("Value is {}", x),
+        None => println!("No value"),
     }
+
+    // if let Some(x) = val {
+    //     println!("Value is {}", x)
+    // }
 }
