d76fe96a2a1a22aa16e1715bf80dad0b83c69f21
ISAWASHUN <syun20000616@icloud.com>
2024-04-29 16:49:43 +0900

    モジュールの基本

--- Lesson1/src/main.rs
+++ Lesson1/src/main.rs
@@ -1,6 +1,32 @@
-use rand::Rng;
+mod test_module {
+    pub mod sub_module1 {
+        pub fn test1_fn1() {
+            println!("test1_fn1");
+        }
+
+        fn test2_fn2() {
+            println!("test2_fn1");
+        }
+    }
+
+    mod sub_module2 {
+        pub fn test1_fn1() {
+            println!("test1_fn2");
+        }
+
+        fn test2_fn2() {
+            println!("test2_fn2");
+        }
+    }
+}
+
+use test_module::sub_module1;
 
 fn main() {
-    let random_number: i32 = rand::thread_rng().gen_range(1..101);
-    println!("Random number: {}", random_number);
+    crate::test_module::sub_module1::test1_fn1();
+    self::test_module::sub_module1::test1_fn1();
+    test_module::sub_module1::test1_fn1();
+    test_module::sub_module1::test1_fn1();
+    // test_module::sub_module2::test1_fn1();
+    // test_module::sub_module1::test2_fn2(); // error: function `test2_fn2` is private
 }
